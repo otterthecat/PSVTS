45844e15a0f13feee0c675fe8564258856303398
Otter <otc@otterthecat.com>
2013-08-10 23:41:31 -0500

    Socket IO is now in it's own client file Basic changes to app's css for somewhat better appearance (still a bit fragile)

--- client/app.js
+++ client/app.js
@@ -1,8 +1,7 @@
-// define the socket
-var socket = io.connect('http://localhost:4000');
-
+// Create interface panel modules
+/////////////////////////////////////
 Panels.setSocket(socket);
-Panels.topPanel.setPreviewToggle(document.querySelector('#appHeader button'), document.querySelector('.app') );
+Panels.topPanel.setPreviewToggle(document.querySelector('#appHeader button'), document.querySelector('#main') );
 Panels.filesPanel.setDisplay(document.querySelector('#fileList'), socket);
 Panels.filesPanel.emit('load_files', 'projects');
 Panels.filesPanel.listen('return_file_data', function(data){
@@ -10,6 +9,10 @@ Panels.filesPanel.listen('return_file_data', function(data){
     Panels.filesPanel.generateDisplay(data.files);
 });
 
+
+// Have socket listen for event to
+// create CodeMirror instance
+/////////////////////////////////////
 socket.on('edit_file', function(data){
 
     Editor.create('#codeMirror', data);
---
+++ client/socket.js
@@ -0,0 +1,3 @@
+// define the socket to make use of server/client events
+///////////////////////////////////////////////////////
+var socket = io.connect('http://localhost:4000');
