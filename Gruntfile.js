8f3ef9a5bb97dfacd1b4881dfef0ad52a72083d8
Otter <otc@otterthecat.com>
2013-09-22 15:16:59 -0500

    Added Grunt watch task to compile LESS files & Browserify JS files

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,15 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+
     pkg: grunt.file.readJSON('package.json'),
 
     jshint: {
 
       files: ['client/*.js', 'server/*/*.js', 'test/specs/*.js'],
       options: {
+
+        // seems jsHint doesn't like Mongoose Schemas...
         ignores: ['server/db/*.js']
       }
     },
@@ -28,6 +31,7 @@ module.exports = function(grunt) {
     mochacli: {
 
         options: {
+
             reporter: 'nyan',
             bail: true
         },
@@ -39,7 +43,14 @@ module.exports = function(grunt) {
         generic: {
             src: ['server/*.js'],
             options: {
-                errorsOnly: false, // show only maintainability errors
+
+                /*
+                  @errorsOnly - show only maintainabilty errors
+                  @cycolmatic - typical acceptance is a value of 4 (lower is better)
+                  @halstead - typcial acceptance is 10, (lower is better)
+                  @maintainability - typical acceptance is ~70. Higher is better, 171 max
+                */
+                errorsOnly: false,
                 cyclomatic: 3,
                 halstead: 10,
                 maintainability: 100
@@ -47,29 +58,54 @@ module.exports = function(grunt) {
         },
     },
 
+    less: {
+
+      dev: {
+
+        src: './less/app.less',
+        dest: './css/app.css'
+      }
+    },
+
     csslint: {
 
       options: {
+
         'box-sizing': false,
         'box-model': false,
         'adjoining-classes': false
       },
 
       src: ['css/*.css']
+    },
+
+    watch: {
+
+      css: {
+
+        files: 'less/*',
+        tasks: ['css']
+      },
+
+      js: {
+
+        files: 'client/*.js',
+        tasks: ['js']
+      }
     }
   });
 
   // Load Grunt Tasks
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-mocha-cli');
   grunt.loadNpmTasks('grunt-complexity');
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-browserify');
 
-  // Default task(s).
+  // Defined task(s).
   grunt.registerTask('default', ['jshint', 'mochacli', 'complexity', 'csslint']);
-  grunt.registerTask('js', ['jshint', 'mochacli', 'complexity']);
-  grunt.registerTask('css', ['csslint']);
-  grunt.registerTask('compile', ['browserify']);
-
+  grunt.registerTask('js', ['jshint', 'mochacli', 'complexity', 'browserify']);
+  grunt.registerTask('css', ['less:dev', 'csslint']);
 };
